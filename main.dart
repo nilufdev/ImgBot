a96f463cd8dab673163231d532249c3c7af3e959
NIHAL <nilufdev@gmail.com>
2025-08-10 15:27:07 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ class ImgBot extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BotChat();
+      home: BotChat(),
     );
   }
 }
