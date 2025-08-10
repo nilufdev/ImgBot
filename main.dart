c28674ad75f9644c8ce749d1f34bedba3d43b324
NIHAL <nilufdev@gmail.com>
2025-08-10 15:00:47 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ImgBot());
+}
+
+class ImgBot extends StatelessWidget {
+  const ImgBot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BotChat();
+    );
+  }
+}
